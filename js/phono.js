a08aec0a0969ed1097685db1f6f1559b47528091
rubelsarker0 <rubelsarker0@outlook.com>
2022-03-01 14:31:01 +0000

    displaying-phone-details

--- js/phono.js
+++ js/phono.js
@@ -37,3 +37,42 @@ const getUserPhones = async () => {
 		console.log(error);
 	}
 };
+
+const displayUserPhone = (phoneData) => {
+	if (phoneData.length === 0) {
+		phoneContainer.textContent = '';
+		errorMessage.textContent = `Result not found!! try again..`;
+		errorMessage.classList.remove('d-none');
+	} else {
+		phoneContainer.textContent = '';
+		phoneDetailContainer.textContent = '';
+		errorMessage.classList.add('d-none');
+		phoneData.slice(0, 20).forEach((phone) => {
+			generatePhoneHTML(phone);
+		});
+	}
+};
+
+const generatePhoneHTML = ({ brand, image, phone_name, slug }) => {
+	const div = document.createElement('div');
+	div.classList.add('col');
+	div.innerHTML = `
+            <div class="card border-success border-bottom border-3 border-0 rounded">
+            <div class="d-flex justify-content-center">
+                <img src="${image}" class="card-img-top w-50 p-3" alt="phone image" />
+            </div>
+            <div class="card-body">
+                <h5 class="card-title text-success">${phone_name}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"></li>
+                    <li class="list-group-item">Brand: ${brand}</li>
+                    <li class="list-group-item">Slug: ${slug}</li>
+                    <li class="list-group-item"></li>
+                </ul>
+                <div class="d-flex align-items-center justify-content-center">
+                    <a id="btn-search" onclick="loadPhoneDetail('${slug}')" class="btn btn-success text-uppercase px-4">Details</a>
+                </div>
+            </div>
+        </div>`;
+	phoneContainer.appendChild(div);
+};
